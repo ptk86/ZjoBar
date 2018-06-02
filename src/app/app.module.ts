cb6719fdfdda3a4491c318b6f2bda6850634e602
Piotr Kiliński <piotr.kilinski@gmail.com>
2018-06-02 10:55:00 +0200

    Add Schedule component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { MenuModule } from 'primeng/menu';
+import { ScheduleModule } from 'primeng/schedule';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { ScheduleComponent } from './schedule/schedule.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
+    ScheduleComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MenuModule,
+    ScheduleModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/schedule/schedule.component.ts
@@ -0,0 +1,53 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-schedule',
+  template: `
+  <div style="width: 50%">
+      <p-schedule [events]="events" [editable]="true" locale="pl"  (onDayClick)=onAddEvent() ></p-schedule>
+  </div>
+  `,
+  styles: []
+})
+export class ScheduleComponent implements OnInit {
+
+  events: any[];
+
+    ngOnInit() {
+        this.events = [
+            {
+                'title': 'All Day Event',
+                'start': '2016-01-01'
+            },
+            {
+                'title': 'Long Event',
+                'start': '2018-05-28',
+                'end': '2018-06-02'
+            },
+            {
+                'title': 'Repeating Event',
+                'start': '2016-01-09T16:00:00'
+            },
+            {
+                'title': 'Repeating Event',
+                'start': '2016-01-16T16:00:00'
+            },
+            {
+                'title': 'Conference',
+                'start': '2016-01-11',
+                'end': '2016-01-13'
+            }
+        ];
+    }
+
+    onAddEvent(){
+      this.events.push(
+        {
+          'title': 'Long Event',
+          'start': '2018-05-28',
+          'end': '2018-06-02'
+        }
+      );
+    }
+
+}
